bff2b14da91b9ddee546a3b9f544d561d352418a
Mirco <mirco.cotugno@gmail.com>
2024-03-22 14:51:01 +0100

    Add loading template for private pages

---
+++ app/playground/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <main className="p-4 md:p-10 mx-auto max-w-7xl min-h-screen flex items-center justify-center">
+      <p className="text-base">Loading ...</p>
+    </main>
+  );
+}
