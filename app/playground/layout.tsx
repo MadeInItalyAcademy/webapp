93d471c849f26d94829a41b7046003e3360ea971
Mirco <mirco.cotugno@gmail.com>
2024-03-22 16:54:11 +0100

    Create user after login if its new

--- app/playground/layout.tsx
+++ app/playground/layout.tsx
@@ -1,4 +1,5 @@
 import { auth } from '@/libs/auth';
+import prisma from '@/libs/prisma';
 import { redirect } from 'next/navigation';
 import { Suspense } from 'react';
 
@@ -9,6 +10,22 @@ export default async function AuthLayout({
 }) {
   const session = await auth();
   if (!session?.user) return redirect('/');
+
+  if (session.user.email) {
+    const userExist = await prisma.user.findUnique({
+      where: { email: session.user.email }
+    });
+
+    if (!userExist) {
+      await prisma.user.create({
+        data: {
+          name: session.user.name || '',
+          email: session.user.email,
+          image: session.user.image || ''
+        }
+      });
+    }
+  }
 
   return <Suspense>{children}</Suspense>;
 }
